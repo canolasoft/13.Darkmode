9354e452938aa32d22d054e545ebf0c77fc30516
Fernando Walpert <walllpapert@gmail.com>
2026-06-17 08:14:24 -0300

    cambios
    - retiradas hojas de estilo sin uso
    - reparado menu desplegable para pantallas pequeñas

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 <title>Modo Oscuro Manual con CSS y JavaScript</title>
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 
 <style>
 /* =========================
--- index2.php
+++ index2.php
@@ -6,6 +6,7 @@
 <title>Darkmode.js - Modo Oscuro para Sitios Web</title>
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 <style>
 .hero{
     background: linear-gradient(135deg,#0f172a,#1e293b);
